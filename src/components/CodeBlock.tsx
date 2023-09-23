6b51fd7398653b10a291e66431e193ed6b7c7c73
Samantha <sam.t.nguyenn@gmail.com>
2023-09-23 00:45:45 -0500

    feat: add copy button to CodeBlock component

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,15 +1,28 @@
 import { SyntaxHighlighterProps } from 'react-syntax-highlighter'
 import SyntaxHighlighter from 'react-syntax-highlighter/dist/esm/default-highlight'
+import { Button } from './Button'
+import { IconClipboard } from '@tabler/icons-react'
 
-export type CodeBlockProps = SyntaxHighlighterProps
-export const CodeBlock = ({children, ...props}: SyntaxHighlighterProps) => {
+export type CodeBlockProps = SyntaxHighlighterProps & {
+	canCopy?: boolean,
+}
+export const CodeBlock = ({
+	children,
+	canCopy = true,
+	...props
+}: SyntaxHighlighterProps) => {
 	return (
-		<SyntaxHighlighter
-			className="p-4 rounded-md bg-slate-100"
-			useInlineStyles={false}
-			{...props}
-		>
-			{children}
-		</SyntaxHighlighter>
+		<div className="flex flex-row p-4 rounded-md bg-slate-100">
+			<SyntaxHighlighter
+				useInlineStyles={false}
+				{...props}
+			>
+				{children}
+			</SyntaxHighlighter>
+			{canCopy && 
+			<Button kind="tertiary" className="p-0 ml-auto bg-[unset] border-0">
+				<IconClipboard size={16} />
+			</Button>}
+		</div>
 	)
 }
