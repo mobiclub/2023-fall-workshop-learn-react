4e6dd1e347b511f71328255e59ddc1974e63c8b7
Samantha <sam.t.nguyenn@gmail.com>
2023-09-23 19:00:33 -0500

    feat: make KeyValuePair component more customizable

--- src/components/KeyValuePair.tsx
+++ src/components/KeyValuePair.tsx
@@ -1,15 +1,25 @@
 import { ReactNode } from 'react'
 import { SectionProps } from 'react-html-props'
+import { twMerge } from 'tailwind-merge'
 
 export type KeyValuePairProps = SectionProps & {
 	k: ReactNode,
 	v: ReactNode,
+	kClassName?: string,
+	vClassName?: string,
 }
-export const KeyValuePair = ({k, v}: KeyValuePairProps) => {
+export const KeyValuePair = ({
+	k,
+	v,
+	className,
+	kClassName,
+	vClassName,
+	...props
+}: KeyValuePairProps) => {
 	return (
-		<section className="flex flex-col gap-2">
-			<p className='uppercase'>{k}</p>
-			<p className='text-xl'>{v}</p>
+		<section className={twMerge("flex flex-col gap-2", className)} {...props}>
+			<p className={twMerge('uppercase', kClassName)}>{k}</p>
+			<p className={twMerge('text-xl', vClassName)}>{v}</p>
 		</section>
 	)
 }
