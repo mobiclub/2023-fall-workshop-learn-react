2e81098453504ce656675c383484890ad94fea52
Samantha <sam.t.nguyenn@gmail.com>
2023-09-30 14:19:35 -0500

    fix: use SlideSplit where possible, use ordered list semantics

--- src/slides/ComponentsSlide.tsx
+++ src/slides/ComponentsSlide.tsx
@@ -1,31 +1,27 @@
-import { Slide, SlideProps } from '@/components/Slide'
+import { Heading } from '@/components/Heading'
+import { SlideSplit, SlideSplitProps } from '@/components/Slide'
 
-import { Heading } from '../components/Heading'
-
-export type ComponentsSlideProps = SlideProps
+export type ComponentsSlideProps = SlideSplitProps
 export const ComponentsSlide = ({ children, className, slideNum, ...props }: ComponentsSlideProps) => {
 	return (
-		<Slide {...props}>
-			<div className ="grid grid-cols-2 h-full">
-				<section className="bg-slate-50 p-24">
-					<header>
-						<Heading level={3}>
-							Chapter 1
-						</Heading>
-						<Heading level={1}>
-							Components
-						</Heading>
-						<Heading level={2}>
-							A reuable unit of some UI
-						</Heading>
-					</header>
-				</section>
-				<section className="bg-white p-24">
-					<p>1. Functional components</p>
-					<p>2. Component properties</p>
-					<p>3. Component states</p>
-				</section>
-			</div>
-		</Slide>
+		<SlideSplit {...props}
+			column1Content={
+				<header className='flex flex-col gap-2'>
+					<span>Chapter 1</span>
+					<Heading level={1}>
+						Components
+					</Heading>
+					<Heading level={2}>
+						A reuable unit of some UI
+					</Heading>
+				</header>
+			}
+			column2Content={
+				<ol className='list-decimal list-inside'>
+					<li>Functional components</li>
+					<li>Component properties</li>
+					<li>Component states</li>
+				</ol>
+			} />
 	)
 }
--- src/slides/TableOfContentsSlide.tsx
+++ src/slides/TableOfContentsSlide.tsx
@@ -1,30 +1,25 @@
-import { Heading } from '../components/Heading'
-import { Slide, SlideProps } from '../components/Slide'
+import { Heading } from '@/components/Heading'
 
-export type TableOfContentsProps = SlideProps & {
-	slideNum?: {current: number, total: number}
-}
+import { SlideSplit, SlideSplitProps } from '../components/Slide'
+
+export type TableOfContentsProps = SlideSplitProps
 export const TableOfContents = ({ children, className, slideNum, ...props }: TableOfContentsProps) => {
 	return (
-		<Slide {...props}>
-			<div className ="grid grid-cols-2 h-full">
-				<section className="bg-slate-50 p-24">
-					<header>
-						<Heading level={1}>
-							Table of Contents
-						</Heading>
-					</header>
-				</section>
-				<section className="bg-white p-24">
-					<p>01. Preparing your environment</p>
-					<br></br>
-					<p>02. Components</p>
-					<br></br>
-					<p>03. React hooks and state</p>
-					<br></br>
-					<p>04. The Rendering Lifecycle</p>
-				</section>
-			</div>
-		</Slide>
+		<SlideSplit {...props}
+			column1Content={
+				<header>
+					<Heading level={1}>
+						Table of Contents
+					</Heading>
+				</header>
+			}
+			column2Content={
+				<ol className='list-decimal list-inside'>
+					<li>Preparing your environment</li>
+					<li>Components</li>
+					<li>React hooks and state</li>
+					<li>The Rendering Lifecycle</li>
+				</ol>
+			} />
 	)
 }
