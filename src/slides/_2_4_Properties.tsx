36ba4d60bd2be7a6651f1943a65ab509cea928e9
Samantha <sam.t.nguyenn@gmail.com>
2023-10-04 16:25:23 -0500

    fix: nesting in Properties slide

--- src/slides/_2_4_Properties.tsx
+++ src/slides/_2_4_Properties.tsx
@@ -2,13 +2,12 @@ import { SlideHeading } from '@/components/Heading'
 
 export const _2_4_Properties = () => {
 	return (
-		<div className='flex flex-row gap-8'>
-			<section className='flex flex-row gap-8'>
-				<SlideHeading level={2} subHeading='Chapter 2' heading='Properties' />
-				<div className="flex flex-col gap-2 max-w-[70ch]">
-					<p></p> 
-				</div>
-			</section>
+		<div className='flex flex-col gap-8'>
+			<SlideHeading level={2} subHeading='Chapter 2' heading='Properties' />
+			<div className="flex flex-col gap-2 max-w-[70ch]">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tincidunt volutpat interdum. Curabitur hendrerit aliquam auctor. Donec vestibulum ex eget pellentesque semper. Donec auctor bibendum erat, fermentum posuere mauris pulvinar ut.</p>
+				<p>Nullam eu sapien euismod, sagittis neque sit amet, elementum risus.</p>
+			</div>
 		</div>
 	)
 }
